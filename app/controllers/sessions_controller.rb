6df712745f08bd919379883e8ea915efcbb4540e
pantsonsteven <pantsonsteven@gmail.com>
2014-07-06 14:29:55 -0400

    added welcome controller, built out sorcery authentication parameters, created form partials for new/update user new session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+  before_action :require_login, only: [:destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user = login(params[:email], params[:password])
+      redirect_back_or_to user_journals_path, notice: 'Login successful'
+    else
+      flash.now[:alert] = 'Login failed'
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(:users, notice: 'Logged out!')
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
 
+  before_action :require_login, only: [:profile, :edit, :update, :show ]
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root, notice: "Signed up! Please log in."
+    else
+      render :new
+    end
+  end
 
   private
 
